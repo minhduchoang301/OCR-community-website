5d5a667780ab333201ea54c0251aedbdd046f550
minhduchoang301 <minhduchoang301@gmail.com>
2020-08-01 20:37:56 +0700

    fix main.py

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import tensorflow as tf
 from Prepoccess import Preprocess
 
-def main(input_dir: str, output_dir: str, model_path: str):
+def main(input_dir: str, model_path: str): #, output_dir: str
     preprocess = Preprocess()
     model = tf.keras.models.load_model(model_path)
     # Lines segmenting
-    lines = preprocess.segment_all(input_dir)
-    # Words segmenting
-    words = preprocess.crop_word_from_line(lines)
-    # Characters segmenting 
-    chars = preprocess.crop_char_from_word(words)
-
+    lines = preprocess.crop_line_from_img(input_dir)
+    
     # Export character images
     #line_num = 0
     paragraph = ""
-    for line in chars:
+    for line in lines:
         line_str =""
-        for word in line:
+	# Words segmenting
+    	words = preprocess.crop_word_from_line(line)
+        for word in words:
             word_str = "" 
-            for char in word:
+	    # Characters segmenting 
+            chars = preprocess.crop_char_from_word(word)
+            for char in chars:
                 char_str = model.predict(cv2.resize(char, (28,28), interpolation = cv2.INTER_AREA))
                 word_str += char_str
             line_str = line_str + " " + word_str
         paragraph = paragraph + "\n" + line_str
-    print(paragraph)
+    return paragraph
 
 
 if __name__ == '__main__':
     input_dir = input("Image path: ")
-    output_dir = input("Output path: ")
+    #output_dir = input("Output path: ")
     model_path = input("Model path: ")
-	main(input_dir, output_dir)
+    main(input_dir, model_path)
